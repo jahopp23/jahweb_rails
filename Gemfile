e1e137e9a5d71185d0502cc2562408b3893aeb5a
jahopp23 <jahopp@gmail.com>
2016-10-18 05:29:15 -0500

    Initial Commit

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 0.18'
+#gem 'pg', '~> 0.18'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -15,7 +15,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+  gem 'sqlite3', '~> 1.3', '>= 1.3.11'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20160903215954) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "groups", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_groups_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "members", force: :cascade do |t|
@@ -29,8 +26,8 @@ ActiveRecord::Schema.define(version: 20160903215954) do
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_members_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_members_on_user_id", using: :btree
+    t.index ["group_id"], name: "index_members_on_group_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,7 +39,4 @@ ActiveRecord::Schema.define(version: 20160903215954) do
     t.datetime "updated_at",      null: false
   end
 
-  add_foreign_key "groups", "users"
-  add_foreign_key "members", "groups"
-  add_foreign_key "members", "users"
 end
